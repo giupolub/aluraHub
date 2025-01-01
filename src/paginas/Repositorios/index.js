e7e77dcfa6be1f9cf8ed438a623c9c1cfea5c890
Giuseppe Poluboiarinov <giuseppe.po@hotmail.com>
2025-01-01 16:07:20 -0300

    Criadas as funcionalidade para editar um repositório e buscar um repositório pelo nome

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { Text, View, FlatList, TextInput, TouchableOpacity, Alert } from 'react-native';
 import estilos from './estilos';
-import { repositoriosUsuario } from '../../servicos/requisicoes/repositorios';
+import { pegarRepositoriosDoUsuario } from '../../servicos/requisicoes/repositorios';
+import { pegarRepositoriosPeloNome } from '../../servicos/requisicoes/repositorios';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function Repositorios({ route, navigation }) {
     const [repo, setRepo] = useState([]);
+    const [nameRepo, setNameRepo] = useState([]);
+    const estaNaTela = useIsFocused()
 
     useEffect(() => {
         async function retornoRepositorios() {
-            const resultado = await repositoriosUsuario(route.params.id)
+            const resultado = await pegarRepositoriosDoUsuario(route.params.id)
             setRepo(resultado)
         }
         retornoRepositorios()
-    }, [])
+    }, [estaNaTela])
+
+    async function buscaRepositorioPeloNome() {
+        const resultado = await pegarRepositoriosPeloNome(route.params.id, nameRepo)
+        setRepo(resultado)
+        setNameRepo("")
+    }
 
     return (
         <View style={estilos.container}>
@@ -24,20 +34,38 @@ export default function Repositorios({ route, navigation }) {
                 <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
             </TouchableOpacity>
 
+            <TextInput
+                placeholder="Busque pelo nome"
+                autoCapitalize="none"
+                style={estilos.entrada}
+                value={nameRepo}
+                onChangeText={setNameRepo}
+            />
+
+            <TouchableOpacity
+                style={estilos.botao}
+                onPress={buscaRepositorioPeloNome}
+            >
+                <Text style={estilos.textoBotao}>
+                    Buscar
+                </Text>
+            </TouchableOpacity>
+
             <FlatList
                 data={repo}
                 style={{ width: "100%" }}
                 keyExtractor={repo => repo.id}
                 renderItem={({ item }) => (
-                    <TouchableOpacity 
-                    style={estilos.repositorio}
-                    onPress={() => navigation.navigate("InfoRepositorio", {item})}
+                    <TouchableOpacity
+                        style={estilos.repositorio}
+                        onPress={() => navigation.navigate("InfoRepositorio", { item })}
                     >
                         <Text style={estilos.repositorioNome}>{item.name}</Text>
                         <Text style={estilos.repositorioData}>Atualizado em {item.data}</Text>
                     </TouchableOpacity>
                 )}
             />
+
         </View>
     );
 }
--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -1,8 +1,35 @@
 import api from "../api";
 
-export async function repositoriosUsuario(id) {
+export async function pegarRepositoriosDoUsuario(id) {
     try {
         const resultado = await api.get(`/repos?postId=${id}`)
+        return resultado.data
+    }
+    catch (error) {
+        console.log(error)
+        return []
+    }
+}
+
+export async function salvarRepositorioUsuario(id, name, data, postId) {
+    try {
+        await api.put(`/repos/${id}`, {
+            id: id,
+            name: name,
+            data: data,
+            postId: postId
+        })
+        return "sucesso"
+    }
+    catch (error) {
+        console.log(error)
+        return "erro"
+    }
+}
+
+export async function pegarRepositoriosPeloNome(id, name) {
+    try {
+        const resultado = await api.get(`/repos?postId=${id}&name=${name}`)
         return resultado.data
     }
     catch (error) {
